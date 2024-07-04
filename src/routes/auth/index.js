055502cd467c62d5fae30efc538ce91062ce3ff0
n9br <ngbr@gmx.net>
2024-07-04 17:07:33 +0200

    minor changes auth-routes, bcrypt

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -3,6 +3,7 @@ const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 const TodoModel = require("../../database/models/TodoModel");
 const UserModel = require("../../database/models/UserModel");
 const AccessTokens = require("../../services/auth/AccessToken");
+const bcrypt = require("bcrypt");
 
 const AuthRouter = Router();
 
@@ -14,29 +15,62 @@ AuthRouter.post("/login", async (req, res) => {
     res.status(StatusCodes.BAD_REQUEST).send(ReasonPhrases.BAD_REQUEST);
     return;
   }
-  const user = await UserModel.scope("allData").findOne({ where: { email } });
 
-  if (user.password !== password) {
+  const user = await UserModel.scope("allData").findOne({ where: { email } });
+  if (!user) {
+    return res.status(StatusCodes.BAD_REQUEST).send(ReasonPhrases.BAD_REQUEST);
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
     res.status(StatusCodes.UNAUTHORIZED).send(ReasonPhrases.UNAUTHORIZED);
-    return;
   }
   user.password = null;
 
   const myToken = AccessTokens.createAccessToken(user.id);
+  // set Token in Response Header
+  res.cookie("token", myToken, {
+    httpOnly: true,
+    // secure: true,
+    // maxAge: 100000,
+    // signed: True,
+  });
 
   res.status(StatusCodes.OK).json({ user, tokens: { accessToken: myToken } });
 });
 
 AuthRouter.post("/signup", async (req, res) => {
-  const { email, password, name, profileImgUrl } = req.body;
-  if (!email || !password || !name || !profileImgUrl) {
+  // const { email, password, name, profileImgUrl } = req.body;
+  const { email, password, name } = req.body;
+  if (!email || !password || !name) {
     res.status(StatusCodes.BAD_REQUEST).send(ReasonPhrases.BAD_REQUEST);
     return;
   }
-  const user = await UserModel.create({ email, password, name, profileImgUrl });
-  user.password = null;
-  const myToken = AccessTokens.createAccessToken(user.id);
-  res.status(StatusCodes.OK).json({ user, tokens: { accessToken: myToken } });
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await UserModel.create({
+      email,
+      password: hashedPassword,
+      name,
+    });
+    newUser.password = null;
+    const myToken = AccessTokens.createAccessToken(newUser.id);
+
+    res.cookie("token", myToken, {
+      // set Token in Response Header
+      httpOnly: true,
+      // secure: true,
+      // maxAge: 100000,
+      // signed: True,
+    });
+    res
+      .status(StatusCodes.OK)
+      .json({ newUser, tokens: { accessToken: myToken } });
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.CONFLICT).send(ReasonPhrases.CONFLICT);
+    return;
+  }
 });
 
 module.exports = { AuthRouter };
